00457a44f86020748d0336e375692caa102112a9
James Freeman <sysadmin@hkskies.com>
2017-09-04 16:01:11 +0100

    Added syslog support and a .service file for systemd start and stop.

--- testprog.c
+++ testprog.c
@@ -7,6 +7,8 @@
  * https://gsamaras.wordpress.com/code/eat-spaces-and-newline-c/
  * https://www.pacificsimplicity.ca/blog/simple-read-configuration-file-struct-example
  * http://www.cprogramming.com/tutorial/c/lesson14.html
+ * https://cboard.cprogramming.com/c-programming/155124-using-c-program-append-text-txt-file.html
+ * https://stackoverflow.com/questions/24249369/how-to-write-pid-to-file-on-unix
  */
 
 #include <stdio.h>
@@ -14,11 +16,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <syslog.h>
 char s[11] = {"\nabcdefghij"};
 
 #define FILENAME "testprog.conf"
 #define MAXBUF 1024
 #define DELIM "="
+#define PIDFILE "/var/run/testprog.pid"
 
 struct config
 {
@@ -65,11 +69,21 @@ struct config get_config(char *filename)
 int main(int argc, char *argv[] )
 {
         struct config configstruct;
-       
+
+	setlogmask (LOG_UPTO (LOG_NOTICE));
+	openlog ("testprog", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);       
 
 	if (argc < 2) {
 		// No arguments were passed
         	configstruct = get_config(FILENAME);
+        	printf("Using configuration file: %s\n", FILENAME);
+		syslog (LOG_NOTICE, "Using configuration file: %s\n", FILENAME);
+		// We assume argv[2] is a pidfile to open
+		FILE *file = fopen( PIDFILE, "wb" );
+		fprintf(file, "%d\n", getpid());
+		fclose(file);
+		printf("Wrote PID to %s\n", PIDFILE );
+		syslog (LOG_NOTICE, "Wrote PID to %s\n", PIDFILE );
 	} else {
 	       	// We assume argv[1] is a config file to open
 	       	FILE *file = fopen( argv[1], "r" );
@@ -78,16 +92,23 @@ int main(int argc, char *argv[] )
 	       	if ( file == 0 )
 	       	{
 	       		printf( "Could not open file\n" );
+			syslog (LOG_CRIT, "Could not open file\n" );
 	       	}
 	       	else 
 	       	{
 			configstruct = get_config(argv[1]);
+        		printf("Using configuration file: %s\n", argv[1]);
+			syslog (LOG_NOTICE, "Using configuration file: %s\n", argv[1]);
 		}
 		fclose(file);
-	}
 
-        /* Struct members */
-        printf("%s12",configstruct.outputfile);
+		// We assume argv[2] is a pidfile to open
+		FILE *pidfile = fopen( argv[2], "wb" );
+		fprintf(pidfile, "%d\n", getpid());
+		fclose(pidfile);
+		printf("Wrote PID to %s\n", argv[2] );
+		syslog (LOG_NOTICE, "Wrote PID to %s\n", argv[2] );
+	}
 
 	/* Code to trim our string and prevent bad chars in filename */
       	char* outputfilePtr = configstruct.outputfile;
@@ -95,18 +116,20 @@ int main(int argc, char *argv[] )
 	outputfilePtr += strspn(outputfilePtr, "\t\n\v\f\r ");
 	len = strcspn(configstruct.outputfile, "\r\n");
 	configstruct.outputfile[len] = '\0';
-        printf("%s12",configstruct.outputfile);
+
+	printf("Writing output to: %s\n",configstruct.outputfile);
+	syslog (LOG_NOTICE, "Writing output to: %s\n",configstruct.outputfile);
 	
  
         /* Cast port as int */
         int x;
         x = atoi(configstruct.loopcount);
-        printf("%d\n",x+1);
+        printf("Iteration count: %d\n",x);
+	syslog (LOG_NOTICE, "Iteration count: %d\n",x);
 
 	int i = 0;
 	while(i < x || x < 0)
 	{
-		printf("Hello World!");
 		FILE *file = fopen(configstruct.outputfile, "a");
 		fputs("\nHello World",file);
 		fputs(s,file);
